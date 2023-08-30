13e508b1651e330e65f06a356c575326ac8b0375
Gabriel Y. Honji <gabriel.honji78@gmail.com>
2023-08-30 08:30:11 -0300

    updateresolvendo os problemas do baena

--- ProjetoTrioPOO/Program.cs
+++ ProjetoTrioPOO/Program.cs
@@ -13,67 +13,60 @@ namespace ProjetoTrioPOO
         static void Main(string[] args)
         {
             // Ex01
-            //Console.WriteLine("Digite o seu salario");
-            //Double salario = Convert.ToInt32(Console.ReadLine());
-            //Console.WriteLine("Digite o seu irrf");
-            //Double irrf = Convert.ToInt32(Console.ReadLine());
-            //Colaborador colaborador = new Colaborador();
-            //colaborador.CalcularSalario(salario);
-            //colaborador.CalcularIRRF(irrf);
-            //Console.WriteLine();
+            Console.WriteLine("Digite o seu salario");
+            Double salario = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Digite o seu irrf");
+            Double irrf = Convert.ToInt32(Console.ReadLine());
+            Colaborador colaborador = new Colaborador();
+            colaborador.CalcularSalario(salario);
+            colaborador.CalcularIRRF(irrf);
+            Console.WriteLine();
 
-            //Professor professor = new Professor();
-            //professor.CalcularSalario(salario);
-            //professor.CalcularIRRF(irrf);
-            //Console.WriteLine();
+            Professor professor = new Professor();
+            professor.CalcularSalario(salario);
+            professor.CalcularIRRF(irrf);
+            Console.WriteLine();
 
-            //Coordenador coordenador = new Coordenador();
-            //coordenador.CalcularSalario(salario);
-            //coordenador.CalcularIRRF(irrf);
-            //Console.WriteLine();
+            Coordenador coordenador = new Coordenador();
+            coordenador.CalcularSalario(salario);
+            coordenador.CalcularIRRF(irrf);
+            Console.WriteLine();
 
-            //Diretor diretor = new Diretor();
-            //diretor.CalcularSalario(salario);
-            //diretor.CalcularIRRF(irrf);
+            Diretor diretor = new Diretor();
+            diretor.CalcularSalario(salario);
+            diretor.CalcularIRRF(irrf);
 
             // Ex02
-            //Console.WriteLine("Digite o Valor do Saque");
-            //double saque = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o Valor do Saque");
+            double saque = Convert.ToDouble(Console.ReadLine());
 
-            //Console.WriteLine("Poupança -----------");
-            //Poupanca poupanca = new Poupanca()
+            Console.WriteLine("Poupança -----------");
+            Poupanca poupanca = new Poupanca()
 
-            //poupanca.CalcularImposto(saque);
-            //Console.WriteLine();
+            poupanca.CalcularImposto(saque);
+            Console.WriteLine();
 
-            //Console.WriteLine("Corrente -----------");
-            //Corrente corrente = new Corrente();
+            Console.WriteLine("Corrente -----------");
+            Corrente corrente = new Corrente();
 
-            //corrente.CalcularImposto(saque);
+            corrente.CalcularImposto(saque);
 
-<<<<<<< Updated upstream
             //// Ex03
-            //Atividade3.Taxi taxi = new Atividade3.Taxi();
-            //taxi.CalcGasValue();
+            Atividade3.Taxi taxi = new Atividade3.Taxi();
+            taxi.CalcGasValue();
 
             Atividade3.Moto moto = new Atividade3.Moto();
 
-            //moto.CalcularValorCombustivel();
+            moto.CalcularValorCombustivel();
             moto.CalcularTotal();
-<<<<<<< HEAD
 
             Atividade3.Uber uber = new Atividade3.Uber();
             Console.WriteLine("Qual uber vc quer usar UberXShare,UberGreen,UberReserve");
             uber.EscolherVeiculo();
             
-=======
-=======
-            // Ex03
             Atividade3.Taxi taxi = new Atividade3.Taxi();
             taxi.CalcularValorCombustivel();
             taxi.CalcularTotal();
->>>>>>> Stashed changes
->>>>>>> f54c7f89a52f1858d3a2ccfbe5e39d9054c385cc
         }
     }
 }
